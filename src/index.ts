9fa3c225600ac7bb2b928bc41f9880698cc9af19
Rust Ross <rust.91@icloud.com>
2020-10-24 21:03:24 +0200

    ts-graphql

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import microConfig from './mikro-orm.config';
 import express from "express";
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -10,9 +15,15 @@ const main = async () => {
 
     const app = express();
     
-    app.get('/', (_, res) => {
-        res.send("hi");
-    })
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false,
+        }),
+        context: () => ({em: orm.em})
+    });
+
+    apolloServer.applyMiddleware({ app });
     
     app.listen(4000, () => {
         console.log('server: localhost:4000');
